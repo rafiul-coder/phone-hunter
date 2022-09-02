80c5ff0ad281bb029c53d45e9e5d5260a65ad6da
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-09-03 02:12:55 +0600

    finished

--- js/summary.js
+++ js/summary.js
@@ -2,8 +2,11 @@
 FETCH
 1. Fetch must provide url
 dynamic or static
+
 2. how to convert fetch promise return to json 
+
 3. how to convert json to data
+
 4. [cool headed]: 
 users: 
 --> array of objects
@@ -11,6 +14,8 @@ users:
 ---> object with a property called data
 nested object
 ---------------
+
+
 DOM manipulation
 1. get something from the DOM
 2. create element to append to the DOM
